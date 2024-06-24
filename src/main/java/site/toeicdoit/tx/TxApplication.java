eb6d8a96d1ab776a4435ae2a7d49d362c0a74339
chan0326 <qkrdydcksz48@gmail.com>
2024-06-24 18:55:22 +0900

    f

--- src/main/java/site/toeicdoit/tx/TxApplication.java
+++ src/main/java/site/toeicdoit/tx/TxApplication.java
@@ -2,9 +2,10 @@ package site.toeicdoit.tx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TxApplication {
 
